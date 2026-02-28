01d1fd8cc772f5b0e0ae99a1094a8491d9430441
xavier <breuilxavier@gmail.com>
2026-02-28 15:02:41 +0100

    dev: Create a button on landing page

---
+++ src/components/redirection_button.js
@@ -0,0 +1,11 @@
+import Button from 'react-bootstrap/Button';
+
+function RedirectionButton() {
+  return (
+    <>
+      <Button variant="primary">Ticket balance</Button>
+    </>
+  );
+}
+
+export default RedirectionButton;
