dbb9a70c0843e19d05d8d26109f7916619ac7848
xavier <breuilxavier@gmail.com>
2026-03-06 06:43:36 +0100

    dev: Read data from first file.

--- src/components/convertBalanceTickets.js
+++ src/components/convertBalanceTickets.js
@@ -4,18 +4,33 @@ import Button from 'react-bootstrap/Button';
 
 const ConvertBalanceTickets = () => {
   const [selectedFiles, setSelectedFiles] = useState([]);
+  const [downloadDisabled, setDownloadDisabled] = useState(true);
+
+  let fileReader;
 
   const downloadAggregatedData = () => {
     console.log('download data');
   }
 
+const enableDownloading = () => {
+  setDownloadDisabled(false);
+  console.log(fileReader.result);
+}
+
+const filesChanged = event => {
+  console.log(event.target.files[0]);
+  fileReader = new FileReader();
+  fileReader.onloadend = enableDownloading;
+  fileReader.readAsText(event.target.files[0]);
+}
+
   return (
     <>
       <Form.Group controlId="formFile" className="mb-3">
         <Form.Label>Select files</Form.Label>
-        <Form.Control type="file" multiple />
+        <Form.Control type="file" multiple accept=".TXT, .txt" onChange={filesChanged}/>
       </Form.Group>
-      <Button variant="primary" onClick={downloadAggregatedData}>download aggregated data</Button>
+      <Button variant="primary" onClick={downloadAggregatedData} disabled={downloadDisabled}>download aggregated data</Button>
     </>
   );
 }
