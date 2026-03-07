7c9705db234b13865d0bd077267fadbb44893d0b
xavier <breuilxavier@gmail.com>
2026-03-07 07:51:19 +0100

    dev: Read multiple files,

--- src/components/convertBalanceTickets.js
+++ src/components/convertBalanceTickets.js
@@ -6,30 +6,53 @@ import { colTitle, filterData } from '../utils.js';
 
 const ConvertBalanceTickets = () => {
   const [downloadDisabled, setDownloadDisabled] = useState(true);
+  const [rawFilteredData, setRawFilteredData] = useState([Object.keys(colTitle)]);
 
   let fileReader;
 
   const downloadAggregatedData = () => {
     console.log('download data');
+    console.log(rawFilteredData);
   }
 
 const enableDownloading = () => {
+  // TODO: pass filtered data to aggregation function
   setDownloadDisabled(false);
-  const dataArray = [Object.keys(colTitle)]
-  const readerResult = fileReader.result.split('\n');
+}
+
+const addFileData = (data, dataArray) => {
+  // Split file against new line
+  const readerResult = data.split('\n');
   const colToExtract = Object.values(colTitle);
+  // For each line, split against ";" to separate values and
+  // get only relevant ones based one colToExtract
   readerResult.forEach(dataLine => {
     dataArray.push(filterData(dataLine.split(';'), colToExtract));
   });
-  console.log(dataArray);
-  // TODO: pass filtered data to aggregation function
 }
 
 const filesChanged = event => {
+  // FIXME: in case of selecting files after having already processed first batch doesn't reinitialize data, the new batch is added to the second one.
+  // reinitializing rawFilteredData state here doesn't work (probably async stuff)
+  const selectedFiles = event.target.files;
+  const dataArray = [];
   fileReader = new FileReader();
-  fileReader.onloadend = enableDownloading;
-  // TODO: (not necessarily hear) loop over all files.
-  fileReader.readAsText(event.target.files[0]);
+  function readFile(index) {
+    if ( index >= selectedFiles.length ) {
+      // When last file is read, set state with data and enable downloading.
+      setRawFilteredData([...rawFilteredData, ...dataArray]);
+      enableDownloading();
+      return;
+    }
+    const file = selectedFiles[index];
+    fileReader.onload = function(e) {
+      // Once current file is read, add data and read next one.
+      addFileData(e.target.result, dataArray);
+      readFile(index+1)
+    }
+    fileReader.readAsText(file);
+  }
+  readFile(0);
 }
 
   return (
